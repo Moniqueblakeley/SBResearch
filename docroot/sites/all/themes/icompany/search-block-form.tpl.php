222cc85168b63615afefe9dddd5d11686c290d56
adnanrangwala <adnan.rangwala@stonybrook.edu>
2014-04-02 23:57:35 -0400

    src

--- docroot/sites/all/themes/icompany/search-block-form.tpl.php
+++ docroot/sites/all/themes/icompany/search-block-form.tpl.php
@@ -2,7 +2,7 @@
 //$search["search_block_form"] = str_replace('value=""', 'value="search..." onblur="setTimeout(\'closeResults()\',2000); if (this.value == \'\') {this.value = \'\';}"  onfocus="if (this.value == \'search...\') {this.value = \'\';}" ', $search["search_block_form"]);
 
 
-$search["actions"] = '<input id="search-submit-topbar" rel="tooltip" data-placement="bottom" data-original-title="' . t('Click to search') . '" type="image" src="' .  base_path() . path_to_theme() . '/img/search-icon.png" height="14" width="13" border="0" a="Search" />';
+$search["actions"] = '<input id="search-submit-topbar" rel="tooltip" data-placement="bottom" data-original-title="' . t('Click to search') . '" type="image" src="' .  base_path() . path_to_theme() . '/img/search-icon.png" height="14" width="13" border="0" a="" />';
 
 ?>
 
