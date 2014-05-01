cf66625e5d2a693c951f6b0bcbece07ce411c94c
unknown <amukhi@SBRO-342.campus.stonybrook.edu>
2014-05-01 16:50:20 -0400

    add

--- docroot/sites/all/themes/icompany/html.tpl.php
+++ docroot/sites/all/themes/icompany/html.tpl.php
@@ -17,7 +17,7 @@
     <?php } ?>
 
     <?php  include_once drupal_get_path('theme', 'icompany') . "/includes/css_include.php"; ?>
- <!--[if lt IE 10]>
+ <!--[if lt IE 9]>
  <link href="/sites/all/themes/icompany/ie9-fix.css" type="text/css" rel="stylesheet"/>
 					<![endif]-->
 
