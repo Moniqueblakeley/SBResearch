848df4bc28318bc0432cd1d8c9175b0be292331c
unknown <amukhi@SBRO-342.campus.stonybrook.edu>
2014-05-05 14:08:44 -0400

    fix

--- docroot/sites/all/themes/icompany/html.tpl.php
+++ docroot/sites/all/themes/icompany/html.tpl.php
@@ -20,9 +20,7 @@
 	
  <!--[if lt IE 10]>
  <link href="/sites/all/themes/icompany/ie10-fix.css" type="text/css" rel="stylesheet" />
- <script type"text/javascript">alert("hello"); </script>
-
-					<![endif]-->
+<![endif]-->
 
     <!--[if lt IE 7]>
     <![endif]-->
