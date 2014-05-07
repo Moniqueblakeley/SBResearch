31390c05e586b1039f9d9aaac14bb697f6a05700
unknown <amukhi@SBRO-342.campus.stonybrook.edu>
2014-05-07 16:34:16 -0400

    add

--- docroot/sites/all/themes/icompany/html.tpl.php
+++ docroot/sites/all/themes/icompany/html.tpl.php
@@ -4,8 +4,7 @@
 
 <head profile="<?php print $grddl_profile; ?>">
    	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <?php print $head ?>
     <title><?php print $head_title ?></title>
