32199fb5d3d5c98b1e239254a1f7f69db16049f5
unknown <amukhi@ovpr-research07.campus.stonybrook.edu>
2014-05-27 14:43:40 -0400

    add

---
+++ docroot/sites/all/themes/icompany/js1/project_links.js
@@ -0,0 +1,13 @@
+﻿jQuery(document).ready(function(){
+	var projectsContainer = jQuery('ul#projects_container'),
+	projects = {
+		"mightySlider": "http://mightyslider.com/",
+		"iLightBox": "http://ilightbox.net/",
+		"iCarousel&trade;": "http://codecanyon.net/item/icarousel/2527180",
+		"iCarousel&trade; - Wordpress": "http://codecanyon.net/item/icarousel-wordpress/2589154"
+	};
+
+		jQuery.each(projects, function(key, val) {
+			if(key.toLowerCase() != 'mightyslider') projectsContainer.append('<li><a href="'+val+'">'+key+'</a></li>');
+		});
+});
